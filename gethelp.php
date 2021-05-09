ceed6c1dcc8eea3f89af4db981ae0258c9007b20
eqrakhattak <eqra.khattak7@gmail.com>
2021-05-09 07:03:37 +0500

    Edits

---
+++ gethelp.php
@@ -0,0 +1,22 @@
+<?php
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = test_input($_POST["name"]);
+    $email = test_input($_POST["email"]);
+    $message = test_input($_POST["message"]);
+
+    if(!isset($_POST['demo-category'])) {
+        $errorMessage .= "<li>You forgot to select your Category!</li>";
+    }
+
+    if(isset($_POST['submit'])){//to run PHP script on submit
+        if(!empty($_POST['checkbox'])){
+            // Loop to store and display values of individual checked checkbox.
+            foreach($_POST['checkbox'] as $selected){
+                echo $selected."</br>";
+            }
+        }
+    }
+}
+
+?>
