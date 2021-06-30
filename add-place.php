89592972425f5be523de3a191e4db115d365fb1c
eqrakhattak <eqra.khattak7@gmail.com>
2021-06-30 23:46:32 +0500

    Update Search

--- add-place.php
+++ add-place.php
@@ -44,6 +44,20 @@
 					});
 				});
 			});
+
+			$("#searchit").keyup(function () {
+				var value = this.value.toLowerCase().trim();
+
+				$("#countries tr").each(function (index) {
+					if (!index) return;
+					$(this).find("td").each(function () {
+						var id = $(this).text().toLowerCase().trim();
+						var not_found = (id.indexOf(value) == -1);
+						$(this).closest('tr').toggle(!not_found);
+						return not_found;
+					});
+				});
+			});
 		</script>
 
 	</head>
